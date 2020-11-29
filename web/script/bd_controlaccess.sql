2804e30829369061c80a2a16879a688283a48ce8
SinFlooOficial <elmerperezquinones@gmail.com>
2020-11-29 11:28:54 -0500

    add changes

--- web/script/bd_controlaccess.sql
+++ web/script/bd_controlaccess.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 28-11-2020 a las 23:48:29
--- Versión del servidor: 10.4.6-MariaDB
--- Versión de PHP: 7.3.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -13,38 +5,22 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `bd_controlaccess`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `persona`
---
-
 CREATE TABLE `persona` (
   `IDPERSONA` int(11) NOT NULL,
-  `DNI` varchar(8) NOT NULL,
-  `NOMBRES` varchar(60) NOT NULL,
-  `APELLIDOS` varchar(100) NOT NULL,
-  `TELEFONO` varchar(11) NOT NULL,
-  `CORREO` varchar(100) NOT NULL
+  `IDTIPODOC` int(11) DEFAULT NULL,
+  `NRODOC` varchar(11) DEFAULT NULL,
+  `NOMBRES` varchar(60) DEFAULT NULL,
+  `APELLIDOS` varchar(100) DEFAULT NULL,
+  `TELEFONO` varchar(11) DEFAULT NULL,
+  `CORREO` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `persona`
 --
 
-INSERT INTO `persona` (`IDPERSONA`, `DNI`, `NOMBRES`, `APELLIDOS`, `TELEFONO`, `CORREO`) VALUES
-(1, '12345678', 'JUAN ARIAS', 'ALIAGA VEGA', '99999999', 'juan@gmail.com'),
-(2, '98765432', 'Maria Bellido', 'Vega Soto', '847482892', 'maria@gmail.com'),
-(4, '4', '4', '4', '4', '4');
+INSERT INTO `persona` (`IDPERSONA`, `IDTIPODOC`, `NRODOC`, `NOMBRES`, `APELLIDOS`, `TELEFONO`, `CORREO`) VALUES
+(1, 1, '12345678', 'Maria Eva', 'Castillo Suarez', '988251450', 'maria@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -69,15 +45,36 @@ INSERT INTO `rol` (`IDROL`, `DESCRIPCION`, `ESTADO`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `tipodoc`
+--
+
+CREATE TABLE `tipodoc` (
+  `IDTIPODOC` int(11) NOT NULL,
+  `TIPO` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tipodoc`
+--
+
+INSERT INTO `tipodoc` (`IDTIPODOC`, `TIPO`) VALUES
+(1, 'DNI'),
+(2, 'CARNET E.'),
+(3, 'RUC'),
+(4, 'PASAPORTE');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuario`
 --
 
 CREATE TABLE `usuario` (
   `IDUSUARIO` int(11) NOT NULL,
-  `USUARIO` varchar(50) NOT NULL,
-  `PASSWORD` varchar(50) NOT NULL,
-  `ESTADO` int(1) NOT NULL,
-  `IDPERSONA` int(11) NOT NULL,
+  `USUARIO` varchar(50) DEFAULT NULL,
+  `PASSWORD` varchar(50) DEFAULT NULL,
+  `ESTADO` int(1) DEFAULT NULL,
+  `IDPERSONA` int(11) DEFAULT NULL,
   `IDROL` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -86,8 +83,7 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`IDUSUARIO`, `USUARIO`, `PASSWORD`, `ESTADO`, `IDPERSONA`, `IDROL`) VALUES
-(1, 'admin', 'admin', 1, 1, 1),
-(2, 'maria', '123', 1, 2, 2);
+(1, 'admin', 'admin', 1, 1, 1);
 
 --
 -- Índices para tablas volcadas
@@ -97,13 +93,20 @@ INSERT INTO `usuario` (`IDUSUARIO`, `USUARIO`, `PASSWORD`, `ESTADO`, `IDPERSONA`
 -- Indices de la tabla `persona`
 --
 ALTER TABLE `persona`
-  ADD PRIMARY KEY (`IDPERSONA`);
+  ADD PRIMARY KEY (`IDPERSONA`),
+  ADD KEY `PERSONA-TIPODOC` (`IDTIPODOC`);
 
 --
 -- Indices de la tabla `rol`
 --
 ALTER TABLE `rol`
   ADD PRIMARY KEY (`IDROL`);
+
+--
+-- Indices de la tabla `tipodoc`
+--
+ALTER TABLE `tipodoc`
+  ADD PRIMARY KEY (`IDTIPODOC`);
 
 --
 -- Indices de la tabla `usuario`
@@ -121,7 +124,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `persona`
 --
 ALTER TABLE `persona`
-  MODIFY `IDPERSONA` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `IDPERSONA` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34571;
 
 --
 -- AUTO_INCREMENT de la tabla `rol`
@@ -130,14 +133,26 @@ ALTER TABLE `rol`
   MODIFY `IDROL` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT de la tabla `tipodoc`
+--
+ALTER TABLE `tipodoc`
+  MODIFY `IDTIPODOC` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `IDUSUARIO` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `IDUSUARIO` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD CONSTRAINT `PERSONA-TIPODOC` FOREIGN KEY (`IDTIPODOC`) REFERENCES `tipodoc` (`IDTIPODOC`);
 
 --
 -- Filtros para la tabla `usuario`
@@ -146,7 +161,3 @@ ALTER TABLE `usuario`
   ADD CONSTRAINT `USER-PERSONA` FOREIGN KEY (`IDPERSONA`) REFERENCES `persona` (`IDPERSONA`),
   ADD CONSTRAINT `USER-ROL` FOREIGN KEY (`IDROL`) REFERENCES `rol` (`IDROL`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
